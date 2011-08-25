6945c66a1ccb545c7b39c8c6929b6c07110819ef
dmitry <dmitry@igps.by>
2011-08-25 15:48:53 +0300

    'new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '5002146edd83525f32861bf7b2ee4c42cd9744ae1a0803fd8149ae595b7d671dc819e1301a65d1195b06a5ee3bdf51f29e719f52e8c364c985fe29dfd88d9425'
